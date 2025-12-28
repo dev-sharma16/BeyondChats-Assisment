550d37c724299bb22f39830cb84305da378a2376
Dev Sharma <devsharma1683@gmail.com>
2025-12-28 20:29:33 +0530

    enabled the route in app file

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
+
 import express from 'express';
 import cors from 'cors';
+import articleRoutes from './routes/article.routes.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/articles', (res) => {
-  return res.status(200);
-});
+app.use('/api/articles', articleRoutes);
 
 export default app;
